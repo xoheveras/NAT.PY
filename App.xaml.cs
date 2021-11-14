bb67d1ea0fb109899884e86cdc563de33d60fb0d
Egor Udovin <57674283+xoheveras@users.noreply.github.com>
2021-11-14 18:53:58 +0300

    Update Interface
    Replacing the old interface

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace NAT.PY
+namespace Test
 {
     /// <summary>
     /// Логика взаимодействия для App.xaml
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набор атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("NAT.PY")]
+[assembly: AssemblyTitle("Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NAT.PY")]
+[assembly: AssemblyProduct("Test")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
