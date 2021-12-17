bebce08ea74ce363346f84bb05d6e4b59bf59843
Egor Udovin <57674283+xoheveras@users.noreply.github.com>
2021-12-17 04:50:46 +0300

    Fixed bug

--- core/controls/NatPyEditor/NatPyEditor.cs
+++ core/controls/NatPyEditor/NatPyEditor.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,6 +19,30 @@ namespace NATPY
         #region Vars
 
         //LinesCount lineCounter = new LinesCount();
+
+        #endregion
+
+        #region Fixed
+
+        private const int WM_USER = 0x0400;
+        private const int EM_SETEVENTMASK = (WM_USER + 69);
+        private const int WM_SETREDRAW = 0x0b;
+        private IntPtr OldEventMask;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam);
+
+        public void BeginUpdate()
+        {
+            SendMessage(this.codeEditor.Handle, WM_SETREDRAW, IntPtr.Zero, IntPtr.Zero);
+            OldEventMask = (IntPtr)SendMessage(this.codeEditor.Handle, EM_SETEVENTMASK, IntPtr.Zero, IntPtr.Zero);
+        }
+
+        public void EndUpdate()
+        {
+            SendMessage(this.codeEditor.Handle, WM_SETREDRAW, (IntPtr)1, IntPtr.Zero);
+            SendMessage(this.codeEditor.Handle, EM_SETEVENTMASK, IntPtr.Zero, OldEventMask);
+        }
 
         #endregion
 
@@ -39,7 +64,9 @@ namespace NATPY
 
         private void EditorTextChanged(object sender, EventArgs e)
         {
+            BeginUpdate();
             highlights.HighlightTexts(this.codeEditor, false);
+            EndUpdate();
         }
     }
 }
