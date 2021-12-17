46a0d277c2d5df0bacd29ffba57357d254b642a0
Egor Udovin <57674283+xoheveras@users.noreply.github.com>
2021-12-17 04:55:56 +0300

    upd fixed

--- core/controls/NatPyEditor/NatPyEditor.cs
+++ core/controls/NatPyEditor/NatPyEditor.cs
@@ -24,26 +24,6 @@ namespace NATPY
 
         #region Fixed
 
-        /*       private const int WM_USER = 0x0400;
-               private const int EM_SETEVENTMASK = (WM_USER + 69);
-               private const int WM_SETREDRAW = 0x0b;
-               private IntPtr OldEventMask;
-
-               [DllImport("user32.dll", CharSet = CharSet.Auto)]
-               private static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam);
-
-               public void BeginUpdate()
-               {
-                   SendMessage(this.codeEditor.Handle, WM_SETREDRAW, IntPtr.Zero, IntPtr.Zero);
-                   OldEventMask = (IntPtr)SendMessage(this.codeEditor.Handle, EM_SETEVENTMASK, IntPtr.Zero, IntPtr.Zero);
-               }
-
-               public void EndUpdate()
-               {
-                   SendMessage(this.codeEditor.Handle, WM_SETREDRAW, (IntPtr)1, IntPtr.Zero);
-                   SendMessage(this.codeEditor.Handle, EM_SETEVENTMASK, IntPtr.Zero, OldEventMask);
-               }*/
-
         [System.Runtime.InteropServices.DllImport("user32.dll")]
         private extern static IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wp, IntPtr lp);
 
